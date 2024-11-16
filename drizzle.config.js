642069ec5295a641f04cdcaaddae78a1c09279d8
Laiba_012 <laibahameed012@gmail.com>
2024-11-16 15:27:21 +0500

    database configured

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,8 +4,8 @@ export default defineConfig({
     out: "./drizzle",
     dialect: "postgresql",
     schema: "./utils/schema.jsx",
-    driver: "pglite",
+    // driver: "postgresql", // Replace "pg" with the correct driver name
     dbCredentials: {
-        url: DATABASE_URL,
-    }
+        url: process.env.NEXT_PUBLIC_DATABASE_URL,
+    },
 });
